b27ca5516c32e62258f2b8f9e8b57620195ed71e
zachary.douglas.scott@gmail.com <zachary.douglas.scott@gmail.com>
2024-10-07 13:23:49 -0700

    add exactly 1-8

---
+++ timer-odds-1-8.py
@@ -0,0 +1,34 @@
+import random
+import math
+
+def time():
+    # time = random.randint(0, 7200000)
+    time = random.randint(3600000, 7200000)
+    hour = math.floor(time / 3600000)
+    minute = math.floor((time % 3600000) / 60000)
+    second = math.floor((time % 60000) / 1000)
+    millisecond = time % 1000
+    hour = str(hour).zfill(1)
+    minute = str(minute).zfill(2)
+    second = str(second).zfill(2)
+    millisecond = str(millisecond).zfill(3)
+    digit_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    digit_str = hour + minute + second + millisecond
+    for digit in digit_str:
+        digit_counts[int(digit)] += 1
+    for count in digit_counts:
+        if count > 1:
+            return False
+    if digit_counts[9] > 0 or digit_counts[0] > 0:
+        return False
+    return True
+
+success_count = 0
+for i in range(10000000):
+    if time():
+        success_count += 1
+
+print("Total successes:", success_count)
+print("Success rate:", success_count / 1000000)
+
+
